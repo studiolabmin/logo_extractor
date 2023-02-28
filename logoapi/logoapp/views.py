6953d1598b62f4ff1d6d1305de972b6d311c0e8d
studiolabmin <min@studiolab.ai>
2023-02-28 15:10:49 +0900

    image download src modified

--- logoapi/logoapp/views.py
+++ logoapi/logoapp/views.py
@@ -59,9 +59,9 @@ def download_images(request):
     buffer = BytesIO()
     with zipfile.ZipFile(buffer, "w") as z:
         for image_path in image_paths:
-            z.write("/static/"+image_path)
-        for image_path in manipulated_image_paths:
             z.write(image_path)
+        for image_path in manipulated_image_paths:
+            z.write("/static/"+image_path)
     buffer.seek(0)
     response.write(buffer.read())
         
