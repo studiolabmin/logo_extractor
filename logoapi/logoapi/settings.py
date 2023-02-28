3607ac0a504003a27dd19eb9cb45d5e6d40965b3
studiolabmin <min@studiolab.ai>
2023-02-28 14:46:58 +0900

    Update img path

--- logoapi/logoapi/settings.py
+++ logoapi/logoapi/settings.py
@@ -116,4 +116,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
