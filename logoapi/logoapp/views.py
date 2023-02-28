8ad15ff6eddac78cba7ca32c47fa28b6eb083801
studiolabmin <min@studiolab.ai>
2023-02-28 10:48:12 +0900

    python-> python3

--- logoapi/logoapp/views.py
+++ logoapi/logoapp/views.py
@@ -12,46 +12,31 @@ import zipfile
 def get_images(request):
     url = request.GET.get('url')
     # Call crawler.py and get the list of image paths
-    cmd = ['python', './logoapp/src/crawler.py', url]
-    #p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    #out, err = p.communicate()
-    
+    cmd = ['python3', './logoapp/src/crawler.py', url]
     out=subprocess.run(cmd, stdout=subprocess.PIPE)
-    print(f"out.stdout: {out.stdout}")
     image_paths = out.stdout.decode().strip().split('\n')
     image_paths = [i.strip() for i in image_paths]
-    # Call remove_background.py for each image and get the list of manipulated image paths
     manipulated_image_paths = []
     for ori_path in image_paths:
-        print(f"input: {ori_path}")
         new_path = 'images/'+'manipulated_' + os.path.basename(ori_path)
         manipulated_image_paths.append(new_path)
-        cmd = ['python', './logoapp/src/remove_bg.py', ori_path, new_path]
-        # p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        # out, err = p.communicate()
+        cmd = ['python3', './logoapp/src/remove_bg.py', ori_path, new_path]
         out=subprocess.run(cmd).stdout
     return JsonResponse({'image_paths': manipulated_image_paths})
 
 def download_images(request):
     url = request.GET.get('url')
     # Call crawler.py and get the list of image paths
-    cmd = ['python', './logoapp/src/crawler.py', url]
-    #p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    #out, err = p.communicate()
-    
+    cmd = ['python3', './logoapp/src/crawler.py', url]    
     out=subprocess.run(cmd, stdout=subprocess.PIPE)
-    print(f"out.stdout: {out.stdout}")
     image_paths = out.stdout.decode().strip().split('\n')
     image_paths = [i.strip() for i in image_paths]
-    # Call remove_background.py for each image and get the list of manipulated image paths
+    
     manipulated_image_paths = []
     for ori_path in image_paths:
-        print(f"input: {ori_path}")
         new_path = 'images/'+'manipulated_' + os.path.basename(ori_path)
         manipulated_image_paths.append(new_path)
-        cmd = ['python', './logoapp/src/remove_bg.py', ori_path, new_path]
-        # p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        # out, err = p.communicate()
+        cmd = ['python3', './logoapp/src/remove_bg.py', ori_path, new_path]
         out=subprocess.run(cmd).stdout
         
         
